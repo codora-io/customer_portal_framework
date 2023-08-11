283add0a7e8264f439d3ac44be93c4a3e0f4d0f9
Muhammad Usman <135799974+usman-codora1@users.noreply.github.com>
2023-08-11 18:38:00 +0500

    Update APIController.php

--- src/Controllers/APIController.php
+++ src/Controllers/APIController.php
@@ -1,1 +1,34 @@
 
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use SonarSoftware\CustomerPortalFramework\Controllers\AccountAuthenticationController;
+use App\Http\Controllers\Controller;
+use App\UsernameLanguage;
+use SonarSoftware\CustomerPortalFramework\Exceptions\AuthenticationException;
+use App\Http\Requests\AuthenticationRequest;
+use SonarSoftware\CustomerPortalFramework\Helpers\HttpHelper;
+
+
+class APIController extends Controller
+{
+
+        public function loginAPI(AuthenticationRequest $request)
+        {
+                $httpHelper = new HttpHelper();
+                $result = $httpHelper->post("customer_portal/auth", ['username' => $request->input('username'), 'password' => $request->input('password')]);
+                if($result)
+                {
+                        return response()->json([
+                                'status' => true,
+                                'user' => $result
+                        ]);
+                }
+                return response()->json([
+                        'status' => false
+                ]);
+        }
+
+}
